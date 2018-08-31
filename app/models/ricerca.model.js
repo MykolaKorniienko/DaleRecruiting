ebc03144e2e4f20c884bb0335480fd88af411bbc
nikolaikornienko <nikolaikornienko.job@gmail.com>
2018-08-31 17:57:11 +0200

    fixed wrong field in ricerca table

--- app/models/ricerca.model.js
+++ app/models/ricerca.model.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, Sequelize) => {
         etaA: {
             type: Sequelize.INTEGER
         },
-        residenza: {
+        residenzaComune: {
             type: Sequelize.STRING
         },
         tipoBadante: {
