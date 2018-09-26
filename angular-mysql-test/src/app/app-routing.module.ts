b65ff04e312b2019f72faa5f823468d694670644
Alexandru Bunduche <alexandru.bunduche@daleconsulting.it>
2018-09-26 11:42:00 +0200

    test

--- angular-mysql-test/src/app/app-routing.module.ts
+++ angular-mysql-test/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ListComponent } from './list/list.component';
-import { EditComponent } from './edit/edit.component';
-import { SearchComponent } from './search/search.component';
-import { SearchListComponent } from './search-list/search-list.component';
+import { ListComponent } from './wiews/list/list.component';
+import { EditComponent } from './wiews/edit/edit.component';
+import { SearchComponent } from './wiews/search/search.component';
+import { SearchListComponent } from './wiews/search-list/search-list.component';
 
 const routes: Routes = [
   { path: 'users', component: ListComponent },
